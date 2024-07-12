bdcda14eb50da60cbb9655219228c15a93f5fd75
dszharikov <dszharikov@yandex.ru>
2024-07-12 18:24:41 +0700

    Add repositories to DI

--- User/Extensions/RepositoryExtensions.cs
+++ User/Extensions/RepositoryExtensions.cs
@@ -1,4 +1,6 @@
+using User.Data.Repositories.ParentRepositories;
 using User.Data.Repositories.SchoolRepositories;
+using User.Data.Repositories.UserRepositories;
 
 namespace User.Extensions;
 
@@ -7,6 +9,8 @@ public static class RepositoryExtensions
     public static IServiceCollection AddRepositories(this IServiceCollection services)
     {
         services.AddScoped<ISchoolRepository, DapperSchoolRepository>();
+        services.AddScoped<IUserRepository, DapperUserRepository>();
+        services.AddScoped<IParentRepository, DapperParentRepository>();
 
         return services;
     }
